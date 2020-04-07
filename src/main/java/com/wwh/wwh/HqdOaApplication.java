af03c2af411f3f54d59c37fdea05161f1f0646dd
wwh <wwh_126.com>
2020-04-07 17:56:56 +0800

    刚刚创建完毕

---
+++ src/main/java/com/wwh/wwh/HqdOaApplication.java
@@ -0,0 +1,13 @@
+package com.wwh.wwh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HqdOaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HqdOaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wwh/wwh/HqdOaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wwh.wwh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HqdOaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
